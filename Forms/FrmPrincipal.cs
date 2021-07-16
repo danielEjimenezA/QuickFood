8c0c9e80aee210af53f97280f7e0e15205955b6a
Darkkeness <brandon_0798qhotmail.com>
2021-07-15 20:49:03 -0500

    Carpetas

--- Forms/FrmPrincipal.cs
+++ Forms/FrmPrincipal.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuickFood
+namespace QuickFood.Forms
 {
-    public partial class Form1 : Form
+    public partial class FrmPrincipal : Form
     {
-        public Form1()
+        public FrmPrincipal()
         {
             InitializeComponent();
         }
