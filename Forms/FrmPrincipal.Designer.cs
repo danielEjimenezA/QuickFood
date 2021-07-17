a1f6257b581a56defdf464a9847164465d2f9a4c
Daniel Jimenez <40795190+danielEjimenezA@users.noreply.github.com>
2021-07-17 18:08:40 -0500

    Subida cambios de correcciones

--- Forms/FrmPrincipal.Designer.cs
+++ Forms/FrmPrincipal.Designer.cs
@@ -29,7 +29,6 @@ namespace QuickFood.Forms
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -42,7 +41,6 @@ namespace QuickFood.Forms
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-=======
             this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
@@ -98,7 +96,6 @@ namespace QuickFood.Forms
             this.crearClienteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.crearClienteToolStripMenuItem.Text = "Crear Cliente";
             this.crearClienteToolStripMenuItem.Click += new System.EventHandler(this.crearClienteToolStripMenuItem_Click);
->>>>>>> 1348ff57058bd6c6d227273348718df68850919c
             // 
             // pedidoToolStripMenuItem
             // 
@@ -120,12 +117,10 @@ namespace QuickFood.Forms
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-<<<<<<< HEAD
             this.Controls.Add(this.button1);
             this.Name = "FrmPrincipal";
             this.Text = "FrmPrincipal";
             this.ResumeLayout(false);
-=======
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.menuStrip2);
             this.MainMenuStrip = this.menuStrip1;
@@ -135,26 +130,19 @@ namespace QuickFood.Forms
             this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
->>>>>>> 1348ff57058bd6c6d227273348718df68850919c
 
         }
 
         #endregion
 
-<<<<<<< HEAD
         private System.Windows.Forms.Button button1;
-=======
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
         private System.Windows.Forms.MenuStrip menuStrip2;
         private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem crearClienteToolStripMenuItem;
-<<<<<<< HEAD
         private System.Windows.Forms.ToolStripMenuItem pedidoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem crearPedidoToolStripMenuItem;
-=======
->>>>>>> 1348ff57058bd6c6d227273348718df68850919c
->>>>>>> bb2f69cfbd9c0cecb4c09413b37b46cce23e68fb
     }
 }
--- Forms/FrmPrincipal.cs
+++ Forms/FrmPrincipal.cs
@@ -19,17 +19,16 @@ namespace QuickFood.Forms
             InitializeComponent();
         }
 
-<<<<<<< HEAD
         private void button1_Click(object sender, EventArgs e)
         {
             FrmPlatoCRUD frmph = new FrmPlatoCRUD();
             frmph.Show();
-=======
+        }
+
         private void crearClienteToolStripMenuItem_Click(object sender, EventArgs e)
         {
             FormCliente fr = new FormCliente();
             fr.Show();
->>>>>>> 1348ff57058bd6c6d227273348718df68850919c
         }
 
         private void crearPedidoToolStripMenuItem_Click(object sender, EventArgs e)
